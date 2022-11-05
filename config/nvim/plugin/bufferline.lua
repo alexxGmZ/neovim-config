cd90269dcccf13285051bfdfce81f43e2fe623d3
al6x <algilbertgomez@gmail.com>
2022-11-05 21:07:44 +0800

    bufferline tabname backward truncation

--- config/nvim/plugin/bufferline.lua
+++ config/nvim/plugin/bufferline.lua
@@ -1,6 +1,20 @@
 require('bufferline').setup {
 	options = {
 		mode = "tabs",
+		truncate_names = false,
+		name_formatter = function(buf)
+			buf.path = string.gsub(buf.path, os.getenv("PWD") .. "/", "")
+			local buf_path_length = #buf.path
+			local max_name_length = 45
+			print(buf.path)
+
+			-- truncate backwards
+			if string.find(buf.path, "/") and buf_path_length > max_name_length  then
+				buf.path = string.sub(buf.path, buf_path_length - max_name_length, buf_path_length)
+				buf.path = "..." .. buf.path
+			end
+			return buf.path
+		end,
 		numbers = "ordinal",
 		indicator = {
 			style = "none"
