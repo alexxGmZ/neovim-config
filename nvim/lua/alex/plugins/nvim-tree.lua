59ac110a0c84b59f417ec4daf8d5251b21cca0ec
alexxShandsome <algilbertgomez@gmail.com>
2023-03-26 22:28:21 +0800

    nvim-tree: display nvim-tree when "nvim ."

--- nvim/lua/alex/plugins/nvim-tree.lua
+++ nvim/lua/alex/plugins/nvim-tree.lua
@@ -1,5 +1,3 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
@@ -37,3 +35,21 @@ require("nvim-tree").setup{
 		},
 	},
 }
+
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
