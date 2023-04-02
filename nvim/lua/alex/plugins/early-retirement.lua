c3659e4963f7733f4469b64499e3d8e57b47fa4e
alexxShandsome <algilbertgomez@gmail.com>
2023-04-02 10:44:37 +0800

    config: nvim-notify and nvim-early-retirement config files

---
+++ nvim/lua/alex/plugins/early-retirement.lua
@@ -0,0 +1,21 @@
+require("early-retirement").setup({
+	-- if a buffer has been inactive for this many minutes, close it
+	retirementAgeMins = 5,
+
+	-- filetypes to ignore
+	ignoredFiletypes = {},
+
+	-- will not close the alternate file
+	ignoreAltFile = true,
+
+	-- will ignore buffers with unsaved changes. If false, the buffers will
+	-- automatically be written and then closed.
+	ignoreUnsavedChangesBufs = true,
+
+	-- ignore non-empty buftypes, e.g. terminal buffers
+	ignoreSpecialBuftypes = true,
+
+	-- uses vim.notify for plugins like nvim-notify
+	notificationOnAutoClose = true,
+})
+
