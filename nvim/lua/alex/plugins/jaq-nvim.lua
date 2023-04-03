bb4095743492490a48c8bd5c56288df2f472dcc3
alexxShandsome <algilbertgomez@gmail.com>
2023-04-04 00:51:19 +0800

    jaq-nvim: config file

---
+++ nvim/lua/alex/plugins/jaq-nvim.lua
@@ -0,0 +1,70 @@
+require('jaq-nvim').setup{
+	cmds = {
+		-- Uses vim commands
+		internal = {
+			vim = "source %"
+		},
+
+		-- Uses shell commands
+		external = {
+			markdown = "glow %",
+			python   = "python3 %",
+			go       = "go run %",
+			sh       = "sh %",
+			lua      = "lua %",
+		}
+	},
+
+	behavior = {
+		-- Default type
+		default     = "terminal",
+
+		-- Start in insert mode
+		startinsert = false,
+
+		-- Use `wincmd p` on startup
+		wincmd      = false,
+
+		-- Auto-save files
+		autosave    = false
+	},
+
+	ui = {
+		float = {
+			-- See ':h nvim_open_win'
+			border    = "rounded",
+
+			-- See ':h winhl'
+			winhl     = "Normal",
+			borderhl  = "FloatBorder",
+
+			-- See ':h winblend'
+			winblend  = 0,
+
+			-- Num from `0-1` for measurements
+			height    = 0.8,
+			width     = 0.8,
+			x         = 0.5,
+			y         = 0.5
+		},
+
+		terminal = {
+			-- Window position
+			position = "bot",
+
+			-- Window size
+			size     = 15,
+
+			-- Disable line numbers
+			line_no  = false
+		},
+
+		quickfix = {
+			-- Window position
+			position = "bot",
+
+			-- Window size
+			size     = 10
+		}
+	}
+}
