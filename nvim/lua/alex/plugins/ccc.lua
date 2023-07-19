4a7dfa204e40134a4f37573e78aabd789508386c
alexxShandsome <algilbertgomez@gmail.com>
2023-07-19 09:26:11 +0800

    feat: config for ccc and git-blame

---
+++ nvim/lua/alex/plugins/ccc.lua
@@ -0,0 +1,9 @@
+local ccc = require("ccc")
+local mapping = ccc.mapping
+
+ccc.setup({
+	highlighter = {
+		auto_enable = true,
+		lsp = true
+	}
+})
---
+++ nvim/lua/alex/plugins/git-blame.lua
@@ -0,0 +1,6 @@
+vim.cmd[[
+	let g:gitblame_enabled = 0
+	let g:gitblame_date_format = "%x %H:%M"
+	let g:gitblame_message_when_not_committed = ""
+	let g:gitblame_message_template = " --> <author> • <date> • <sha>: \"<summary>\""
+]]
