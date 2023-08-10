c8651579e3b8344cb4baa18005d9aa28e150ab08
alexxShandsome <algilbertgomez@gmail.com>
2023-08-10 14:01:19 +0800

    lspconfig: remove navic require

--- nvim/lua/alex/plugins/lsp/lspconfig.lua
+++ nvim/lua/alex/plugins/lsp/lspconfig.lua
@@ -1,4 +1,3 @@
-local navic = require("nvim-navic")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
@@ -23,15 +22,6 @@ local LSP_LIST = {
 	"lemminx",
 	"quick_lint_js",
 	"tsserver"
-}
-
---
--- nvim navic lsp setup
---
-navic.setup{
-	lsp = {
-		auto_attach = true
-	}
 }
 
 --
