3ccc3bc43e7cbe63b7d216bf5dc3b1dbf2e5325a
alexxShandsome <algilbertgomez@gmail.com>
2023-08-10 13:59:31 +0800

    illuminate: add ignore filetypes

--- nvim/lua/alex/plugins/vim-illuminate.lua
+++ nvim/lua/alex/plugins/vim-illuminate.lua
@@ -4,3 +4,14 @@ vim.cmd([[
 	hi link IlluminatedWordWrite Visual
 	hi MatchParen cterm=none gui=underline
 ]])
+
+require("illuminate").configure({
+	filetypes_denylist = {
+		"help",
+		"text",
+		"man",
+		"markdown",
+		"checkhealth",
+		"NvimTree"
+	},
+})
