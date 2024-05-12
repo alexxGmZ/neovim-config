cb98a7e1d0e70d6a6d87644904d2caa5203a1fa0
alexxShandsome <algilbertgomez@gmail.com>
2024-05-12 11:52:43 +0800

    keymaps: clear up commented out code

--- nvim/lua/alex/core/keymaps.lua
+++ nvim/lua/alex/core/keymaps.lua
@@ -15,18 +15,6 @@ map.set("n", "N", "Nzzzv")
 map.set("n", "<C-o>", "<C-o>zz")
 map.set("n", "<C-i>", "<C-i>zz")
 
--- nvim-tree
--- map.set("n", "<leader>F", ":NvimTreeToggle<CR>")
--- map.set("n", "<leader>F", function() vim.cmd("NvimTreeToggle") end, { desc = "NvimTreeToggle: NvimTree File Explorer" })
--- map.set("n", "<F6>", function() vim.cmd("NvimTreeToggle") end, { desc = "NvimTreeToggle: NvimTree File Explorer" })
-
--- write and quit
--- map.set("n", "<leader>ww", ":w<CR>", { desc = "Write or Save file" })
--- map.set("n", "<leader>wq", ":wq<CR>", { desc = "Write and Quit file" })
--- map.set("n", "<leader>wa", ":wa<CR>", { desc = "Write all file" })
--- map.set("n", "<leader>qq", ":q<CR>", { desc = "Quit file" })
--- map.set("n", "<leader>qa", ":qa<CR>", { desc = "Quit all file" })
-
 map.set("n", "<leader>ww", function() vim.cmd("w") end, { desc = "Write or Save file" })
 map.set("n", "<leader>wq", function() vim.cmd("wq") end, { desc = "Write and Quit file" })
 map.set("n", "<leader>wa", function() vim.cmd("wa") end, { desc = "Write all file" })
@@ -38,7 +26,6 @@ map.set("n", "<Esc>", function() vim.cmd("noh") end, { desc = "nohlsearch" })
 
 -- fzf
 map.set("n", "<C-p>", function() vim.cmd("FzfLua files") end, { desc = "FzfLua: FzfLua files" })
--- map.set("n", "<C-p>", function () require("fzf-lua").files() end)
 
 -- split resizing
 map.set("n", "<leader>-", "<C-w>3-", { desc = "Pane: Decrease current pane height" })
