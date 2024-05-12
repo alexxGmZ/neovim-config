e5fd3742c583abf7addbd8fbb5208b71366389f2
alexxShandsome <algilbertgomez@gmail.com>
2024-05-12 22:48:17 +0800

    everforest: highlight overrides

--- nvim/lua/alex/plugins/colorscheme/everforest.lua
+++ nvim/lua/alex/plugins/colorscheme/everforest.lua
@@ -10,16 +10,31 @@ return {
 			disable_italic_comments = false,
 			sign_column_background = "none",
 			ui_contrast = "high",
-			dim_inactive_windows = true,
+			dim_inactive_windows = false,
 			diagnostic_text_highlight = false,
 			diagnostic_virtual_text = "coloured",
 			diagnostic_line_highlight = false,
 			spell_foreground = false,
 			show_eob = true,
 			float_style = "dim",
-			---@param highlight_groups Highlights
-			---@param palette Palette
-			on_highlights = function(highlight_groups, palette) end,
+			---@param hl
+			---@param palette
+			on_highlights = function(hl, palette)
+				hl.TSBoolean = { fg = palette.purple, bold = true }
+				hl.TSFloat = { fg = palette.purple, bold = true }
+				hl.TSNumber = { fg = palette.purple, bold = true }
+				hl.TSFunction = { fg = palette.green, bold = true }
+				hl.TSFunctionCall = { fg = palette.green, bold = true }
+				hl.TSKeyword = { fg = palette.red, italic = true }
+				hl.TSParameter = { fg = palette.red, bold = true }
+				hl.TSString = { fg = palette.aqua, bold = true }
+				hl.TSConditional = { fg = palette.red, italic = true }
+				hl.TSVariable = { fg = palette.none, bold = true }
+
+				hl.CurrentWord = { fg = palette.none, underline = true }
+				hl.String = { fg = palette.aqua, bold = true }
+				hl.Keyword = { fg = palette.red, italic = true }
+			end,
 			---@param palette Palette
 			colours_override = function(palette) end,
 		})
