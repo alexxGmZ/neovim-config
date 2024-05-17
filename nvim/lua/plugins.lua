033d6870a664e6b917d164540fb17c29079a4638
alexxShandsome <algilbertgomez@gmail.com>
2024-05-17 20:27:54 +0800

    minor changes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,17 +70,17 @@ return {
 			"InsertEnter"
 		},
 		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-vsnip",
+			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/vim-vsnip",
 			"hrsh7th/vim-vsnip-integ",
 			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"lukas-reineke/cmp-under-comparator",
 			"onsails/lspkind.nvim",
-			"rafamadriz/friendly-snippets",
+			"rafamadriz/friendly-snippets"
 		},
 		config = function()
 			require("alex.plugins.lsp.nvim-cmp")
