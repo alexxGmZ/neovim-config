bc97db95fc69612f5257a7e5330c1133ef5a6e93
alexxShandsome <algilbertgomez@gmail.com>
2024-05-21 10:15:57 +0800

    comment: cleanup

--- nvim/lua/alex/plugins/comment.lua
+++ nvim/lua/alex/plugins/comment.lua
@@ -1,16 +1,7 @@
 return {
 	"numToStr/Comment.nvim",
-	-- event = "VeryLazy",
-	lazy = false,
-	-- keys = {
-	-- 	{ "gb", desc = "Comment: Block comment" },
-	-- 	{ "gc", desc = "Comment: Line comment" },
-	-- 	{ "V" },
-	-- 	-- { "v" },
-	-- },
 	config = function()
 		require("Comment").setup({
-			-- pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
 		})
 	end
 }
