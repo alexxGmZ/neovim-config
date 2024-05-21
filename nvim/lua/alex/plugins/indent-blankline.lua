ab93097feafb7009d8d007960fd7542dc24a8567
alexxShandsome <algilbertgomez@gmail.com>
2024-05-21 20:08:04 +0800

    ibl: cleanup

--- nvim/lua/alex/plugins/indent-blankline.lua
+++ nvim/lua/alex/plugins/indent-blankline.lua
@@ -1,14 +1,8 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	-- event = "VeryLazy",
 	cmd = "IBLEnable",
 	main = "ibl",
 	config = function()
-		-- vim.opt.list = true
-		-- vim.opt.termguicolors = true
-		-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#2c2c2c gui=nocombine]]
-		-- vim.cmd [[highlight IndentBlanklineContextChar guifg=#45475e gui=nocombine]]
-
 		require("ibl").setup()
 	end
 }
