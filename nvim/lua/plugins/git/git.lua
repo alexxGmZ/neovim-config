064662d65140251f03178b0a3631dcccef408e60
alexxGmZ <algilbertgomez@gmail.com>
2024-07-05 18:27:07 +0800

    refactor!: merge all git related plugins

---
+++ nvim/lua/plugins/git/git.lua
@@ -0,0 +1,32 @@
+return {
+   {
+      "lewis6991/gitsigns.nvim",
+      event = "VeryLazy",
+      config = function()
+         require("gitsigns").setup {
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+               virt_text = true,
+               virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+               delay = 500,
+               ignore_whitespace = false,
+            },
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>, <abbrev_sha> - \"<summary>\"",
+         }
+      end
+   },
+   {
+      "idanarye/vim-merginal",
+      cmd = { "Merginal", "MerginalToggle", "Git", "G" },
+      dependencies = { "tpope/vim-fugitive" },
+   },
+   {
+      "akinsho/git-conflict.nvim",
+      cmd = "GitConflictListQf",
+      version = "*",
+      config = function()
+         require("git-conflict").setup()
+      end
+   }
+}
+
