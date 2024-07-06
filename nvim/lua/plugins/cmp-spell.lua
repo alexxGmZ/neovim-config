193be24c0b018f4bfd88acdd8c98584bd28c5c2c
alexxGmZ <algilbertgomez@gmail.com>
2024-07-06 22:45:42 +0800

    cmp-spell: minor changes

--- nvim/lua/plugins/cmp-spell.lua
+++ nvim/lua/plugins/cmp-spell.lua
@@ -1,23 +1,24 @@
 return {
-	"f3fora/cmp-spell",
-	config = function()
-		vim.opt.spell = false
-		vim.opt.spelllang = { 'en_us' }
+   "f3fora/cmp-spell",
+   dependencies = { "hrsh7th/nvim-cmp" },
+   cmd = { "Spell", "SpellToggle" },
+   config = function()
+      vim.opt.spell = false
+      vim.opt.spelllang = { 'en_us' }
 
-		-- enable spell
-		vim.api.nvim_create_user_command("Spell", function()
-			vim.cmd("set spell")
-		end, {})
+      -- enable spell
+      vim.api.nvim_create_user_command("Spell", function()
+         vim.cmd("set spell")
+      end, {})
 
-		-- disble spell
-		vim.api.nvim_create_user_command("NoSpell", function()
-			vim.cmd("set nospell")
-		end, {})
+      -- disble spell
+      vim.api.nvim_create_user_command("NoSpell", function()
+         vim.cmd("set nospell")
+      end, {})
 
-		-- toggle spell
-		vim.api.nvim_create_user_command("SpellToggle", function()
-			vim.cmd("set invspell")
-		end, {})
-	end,
-	cmd = { "Spell", "SpellToggle" }
+      -- toggle spell
+      vim.api.nvim_create_user_command("SpellToggle", function()
+         vim.cmd("set invspell")
+      end, {})
+   end,
 }
