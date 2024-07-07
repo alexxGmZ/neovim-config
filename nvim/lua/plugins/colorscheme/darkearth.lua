e362b3276618ec0c7a0404925a16bc93dbb95bda
alexxGmZ <algilbertgomez@gmail.com>
2024-07-07 12:11:57 +0800

    feat: improve highlight override logic

--- nvim/lua/plugins/colorscheme/darkearth.lua
+++ nvim/lua/plugins/colorscheme/darkearth.lua
@@ -1,3 +1,35 @@
+local function highlight_overrides()
+   local palette = {
+      fg = "#D7C484",
+      bg = "#24211E",
+      red = "#B3664D",
+      orange = "#BB7844",
+      yellow = "#C9A654",
+      green = "#77824A",
+      blue = "#5F865F",
+      brown = "#B36B42"
+   }
+   local set_hl = vim.api.nvim_set_hl
+
+   set_hl(0, "RainbowDelimiterRed", { fg = palette.red })
+   set_hl(0, "RainbowDelimiterYellow", { fg = palette.yellow })
+   set_hl(0, "RainbowDelimiterCyan", { fg = palette.green })
+   set_hl(0, "RainbowDelimiterBlue", { fg = palette.blue })
+   set_hl(0, "RainbowDelimiterOrange", { fg = palette.orange })
+   set_hl(0, "Function", { fg = palette.orange, bold = true })
+   set_hl(0, "String", { fg = palette.yellow, bold = true })
+   set_hl(0, "Statement", { italic = true, bold = false })
+   set_hl(0, "Keyword", { fg = palette.blue, italic = true })
+   set_hl(0, "Identifier", { fg = palette.red, bold = true })
+   set_hl(0, "@variable", { fg = palette.fg, bold = false })
+   set_hl(0, "Number", { fg = palette.yellow, bold = true })
+   set_hl(0, "Type", { fg = palette.orange, bold = false, italic = true })
+   set_hl(0, "Constant", { bold = true })
+   set_hl(0, "Field", { fg = palette.orange, bold = true })
+   set_hl(0, "Operator", { fg = palette.orange, bold = true })
+   set_hl(0, "ColorColumn", { link = "CursorLine" })
+end
+
 return {
    "ptdewey/darkearth-nvim",
    -- lazy = true,
@@ -6,35 +38,18 @@ return {
    config = function()
       vim.cmd.colorscheme "darkearth"
 
-      -- highlight overrides
-      local palette = {
-         fg = "#D7C484",
-         bg = "#24211E",
-         red = "#B3664D",
-         orange = "#BB7844",
-         yellow = "#C9A654",
-         green = "#77824A",
-         blue = "#5F865F",
-         brown = "#B36B42"
-      }
-      local set_hl = vim.api.nvim_set_hl
+      highlight_overrides()
 
-      set_hl(0, "RainbowDelimiterRed", { fg = palette.red })
-      set_hl(0, "RainbowDelimiterYellow", { fg = palette.yellow })
-      set_hl(0, "RainbowDelimiterCyan", { fg = palette.green })
-      set_hl(0, "RainbowDelimiterBlue", { fg = palette.blue })
-      set_hl(0, "RainbowDelimiterOrange", { fg = palette.orange })
-      set_hl(0, "Function", { fg = palette.orange, bold = true })
-      set_hl(0, "String", { fg = palette.yellow, bold = true })
-      set_hl(0, "Statement", { italic = true, bold = false })
-      set_hl(0, "Keyword", { fg = palette.blue, italic = true })
-      set_hl(0, "Identifier", { fg = palette.red, bold = true })
-      set_hl(0, "@variable", { fg = palette.fg, bold = false })
-      set_hl(0, "Number", { fg = palette.yellow, bold = true })
-      set_hl(0, "Type", { fg = palette.orange, bold = false, italic = true })
-      set_hl(0, "Constant", { bold = true })
-      set_hl(0, "Field", { fg = palette.orange, bold = true })
-      set_hl(0, "Operator", { fg = palette.orange, bold = true })
-      set_hl(0, "ColorColumn", { link = "CursorLine" })
+      vim.api.nvim_create_autocmd("ColorScheme", {
+         pattern = "*",
+         group = "HANDSOME",
+         callback = function()
+            if vim.g.colors_name ~= "darkearth" then
+               return
+            end
+
+            highlight_overrides()
+         end
+      })
    end
 }
