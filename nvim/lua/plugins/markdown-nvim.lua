cfb0a8c92f02a8a8602512b03786a38052bb32a9
alexxGmZ <algilbertgomez@gmail.com>
2024-07-09 23:30:46 +0800

    markdown-nvim: foreground highlight override

--- nvim/lua/plugins/markdown-nvim.lua
+++ nvim/lua/plugins/markdown-nvim.lua
@@ -8,6 +8,14 @@ return {
          heading = {
             icons = { "󰬺 ", "󰬻 ", "󰬼 ", "󰬽 ", "󰬾 ", "󰬿 " },
             signs = { "" },
+            foregrounds = {
+               "@markup.heading.1.markdown",
+               "@markup.heading.2.markdown",
+               "@markup.heading.3.markdown",
+               "@markup.heading.4.markdown",
+               "@markup.heading.5.markdown",
+               "@markup.heading.6.markdown"
+            }
          },
       })
    end,
