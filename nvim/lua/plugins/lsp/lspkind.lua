f7b2b6ed0c53b8931afadbba6e8c3356ccc5e6d7
alexxGmZ <algilbertgomez@gmail.com>
2024-07-11 00:10:25 +0800

    lspkind: remove icon overrides

--- nvim/lua/plugins/lsp/lspkind.lua
+++ nvim/lua/plugins/lsp/lspkind.lua
@@ -18,38 +18,11 @@ return {
          -- 'codicons' for codicon preset (requires vscode-codicons font)
          --
          -- default: 'default'
-         preset = 'codicons',
+         preset = 'default',
 
          -- override preset symbols
          --
          -- default: {}
-         symbol_map = {
-            Text = "󰉿",
-            Method = "󰆧",
-            Function = "󰊕",
-            Constructor = "",
-            Field = "󰜢",
-            Variable = "󰀫",
-            Class = "󰠱",
-            Interface = "",
-            Module = "",
-            Property = "󰜢",
-            Unit = "󰑭",
-            Value = "󰎠",
-            Enum = "",
-            Keyword = "󰌋",
-            Snippet = "",
-            Color = "󰏘",
-            File = "󰈙",
-            Reference = "󰈇",
-            Folder = "󰉋",
-            EnumMember = "",
-            Constant = "󰏿",
-            Struct = "󰙅",
-            Event = "",
-            Operator = "󰆕",
-            TypeParameter = "",
-         },
       })
    end
 }
