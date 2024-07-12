9c37b78b78a24a763c549542fdf4954f6442e66d
alexxGmZ <algilbertgomez@gmail.com>
2024-07-13 00:40:45 +0800

    lspkind: highlight overrides

--- nvim/lua/plugins/lsp/lspkind.lua
+++ nvim/lua/plugins/lsp/lspkind.lua
@@ -24,5 +24,36 @@ return {
          --
          -- default: {}
       })
+
+      -- highlight overrides
+      vim.cmd [[
+         highlight! default link CmpItemKindMatch          Boolean
+         highlight! default link CmpItemKindMatchFuzzy     Boolean
+         highlight! default link CmpItemKindText           Label
+         highlight! default link CmpItemKindMethod         Function
+         highlight! default link CmpItemKindFunction       Function
+         highlight! default link CmpItemKindConstructor    Function
+         highlight! default link CmpItemKindField          Type
+         highlight! default link CmpItemKindVariable       Type
+         highlight! default link CmpItemKindClass          Structure
+         highlight! default link CmpItemKindInterface      Structure
+         highlight! default link CmpItemKindModule         Structure
+         highlight! default link CmpItemKindProperty       Type
+         highlight! default link CmpItemKindUnit           Boolean
+         highlight! default link CmpItemKindValue          Character
+         highlight! default link CmpItemKindEnum           Structure
+         highlight! default link CmpItemKindKeyword        Operator
+         highlight! default link CmpItemKindSnippet        Special
+         highlight! default link CmpItemKindColor          Constant
+         highlight! default link CmpItemKindFile           String
+         highlight! default link CmpItemKindReference      Character
+         highlight! default link CmpItemKindFolder         String
+         highlight! default link CmpItemKindEnumMember     Type
+         highlight! default link CmpItemKindConstant       Constant
+         highlight! default link CmpItemKindStruct         Structure
+         highlight! default link CmpItemKindEvent          Conditional
+         highlight! default link CmpItemKindOperator       Operator
+         highlight! default link CmpItemKindTypeParameter  Special
+      ]]
    end
 }
