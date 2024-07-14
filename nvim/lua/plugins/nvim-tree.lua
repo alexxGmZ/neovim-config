377a9282f8bfc70e4932ef9933eb10412d907e5c
alexxGmZ <algilbertgomez@gmail.com>
2024-07-14 20:57:58 +0800

    nvim-tree: remove termguicolors option

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,8 +2,6 @@ return {
    "nvim-tree/nvim-tree.lua",
    cmd = "NvimTreeOpen",
    config = function()
-      vim.opt.termguicolors = true
-
       local HEIGHT_RATIO = 0.8 -- You can change this
       local WIDTH_RATIO = 0.5 -- You can change this too
 
