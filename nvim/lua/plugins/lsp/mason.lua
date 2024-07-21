9a5b5427710bbef98b62effedb9464aa12f223f4
alexxGmZ <algilbertgomez@gmail.com>
2024-07-21 22:50:22 +0800

    mason: fix lazyload by command not working

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,21 +1,21 @@
 return {
-	"williamboman/mason.nvim",
-   cmd = "Mason",
-	config = function()
-		local status, mason = pcall(require, "mason")
-		if not status then
-			return
-		end
+   "williamboman/mason.nvim",
+   cmd = { "Mason" },
+   config = function()
+      local status, mason = pcall(require, "mason")
+      if not status then
+         return
+      end
 
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗"
-				},
-				border = "rounded",
-			}
-		})
-	end
+      mason.setup({
+         ui = {
+            icons = {
+               package_installed = "✓",
+               package_pending = "➜",
+               package_uninstalled = "✗"
+            },
+            border = "rounded",
+         }
+      })
+   end
 }
