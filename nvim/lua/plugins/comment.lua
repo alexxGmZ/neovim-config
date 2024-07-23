33f19abc60e859c5a1f543802eeab93a415376d3
alexxGmZ <algilbertgomez@gmail.com>
2024-07-23 15:08:37 +0800

    comment: lazy load via VeryLazy event

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,8 +1,7 @@
 return {
    "numToStr/Comment.nvim",
-   lazy = false,
+   event = "VeryLazy",
    config = function()
-      require("Comment").setup({
-      })
+      require("Comment").setup({})
    end
 }
