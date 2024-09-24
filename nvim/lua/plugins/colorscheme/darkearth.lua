521ce2981559760c6734299b1589e52145c12f40
alexxGmZ <algilbertgomez@gmail.com>
2024-09-24 21:24:02 +0800

    darkearth: more highlight overrides

--- nvim/lua/plugins/colorscheme/darkearth.lua
+++ nvim/lua/plugins/colorscheme/darkearth.lua
@@ -40,6 +40,8 @@ local function highlight_overrides()
    set_hl(0, "TSParameter", { link = "@parameter" })
    set_hl(0, "@variable.parameter", { link = "@parameter" })
    set_hl(0, "Boolean", { fg = palette.green, bold = true })
+   set_hl(0, "@variable.member.javascript", { bold = true })
+   set_hl(0, "@punctuation.delimiter.javascript", { link = "@variable" })
 
    -- UI
    set_hl(0, "VertSplit", { fg = palette.grey })
