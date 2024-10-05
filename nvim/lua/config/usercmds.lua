6dd9d74fb5b8a074a139187ec8e98d7fe79d3c06
alexxGmZ <algilbertgomez@gmail.com>
2024-10-06 00:28:52 +0800

    usercmd: created Player user command

--- nvim/lua/config/usercmds.lua
+++ nvim/lua/config/usercmds.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_user_command("DiffOrig", function()
+local create_user_command = vim.api.nvim_create_user_command
+
+create_user_command("DiffOrig", function()
    local scratch_buffer = vim.api.nvim_create_buf(false, true)
    local current_ft = vim.bo.filetype
    vim.cmd("vertical sbuffer" .. scratch_buffer)
@@ -9,3 +11,37 @@ vim.api.nvim_create_user_command("DiffOrig", function()
    vim.cmd.wincmd("p")
    vim.cmd.diffthis()       -- current buffer
 end, {})
+
+create_user_command("Player", function(args)
+   local system = vim.fn.system
+   local notify = vim.notify
+   local tool = "playerctl "
+   local arg = args["args"]
+
+   local function notify_new_track()
+      local song = system(tool .. "metadata --format '{{ artist }} - {{ title }}' | tr -d '\n'")
+      notify("󰐊 " .. song)
+   end
+
+   if arg == "status" then
+      local status = system(tool .. "status | tr -d '\n'");
+      notify(status)
+   elseif arg == "next" then
+      system(tool .. "next")
+      notify_new_track()
+   elseif arg == "previous" then
+      system(tool .. "previous")
+      notify_new_track()
+   elseif arg == "pause" then
+      system(tool .. "pause")
+   elseif arg == "play" then
+      system(tool .. "play")
+   elseif arg == "play-pause" then
+      system(tool .. "play-pause")
+   else
+      local player_name = system(tool .. "metadata --format '{{ playerName }}'")
+      local song = system(tool .. "metadata --format '{{ artist }} - {{ title }}' | tr -d '\n'")
+      notify(player_name .. "󰐊 " .. song)
+   end
+end, {nargs = "*"});
+
