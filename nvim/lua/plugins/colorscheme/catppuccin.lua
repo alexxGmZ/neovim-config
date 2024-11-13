2a0c746bf69147f82de61c82cde9f7fd338c2c0a
alexxGmZ <algilbertgomez@gmail.com>
2024-11-13 18:58:54 +0800

    catppuccin: highlight overrides

--- nvim/lua/plugins/colorscheme/catppuccin.lua
+++ nvim/lua/plugins/colorscheme/catppuccin.lua
@@ -16,7 +16,17 @@ return {
             booleans = { "bold" },
             numbers = { "bold" },
             miscs = {},
-         }
+         },
+         custom_highlights = function(colors)
+            return {
+               ["@markup.heading.1.markdown"] = { fg = colors.red, bold = true },
+               ["@markup.heading.2.markdown"] = { fg = colors.peach, bold = true },
+               ["@markup.heading.3.markdown"] = { fg = colors.yellow, bold = true },
+               ["@markup.heading.4.markdown"] = { fg = colors.green, bold = true },
+               ["@markup.heading.5.markdown"] = { fg = colors.blue, bold = true },
+               ["@markup.heading.6.markdown"] = { fg = colors.mauve, bold = true },
+            }
+         end
       })
       vim.cmd.colorscheme "catppuccin"
    end
