6110c2c463ff879f0801684e6ccdb63d3939f151
alexxGmZ <algilbertgomez@gmail.com>
2024-11-13 13:31:13 +0800

    treesitter-context: use opts instead of setup function

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -1,18 +1,16 @@
 return {
    "nvim-treesitter/nvim-treesitter-context",
-   dependencies = { "nvim-treesitter/nvim-treesitter" },
-   config = function()
-      require "treesitter-context".setup {
-         enable = true,
-         max_lines = 0,
-         min_window_height = 0,
-         line_numbers = true,
-         multiline_threshold = 20,
-         trim_scope = "outer",
-         mode = "cursor",
-         separator = nil,
-         zindex = 20,
-         on_attach = nil,
-      }
-   end
+   event = "VeryLazy",
+   opts = {
+      enable = true,
+      max_lines = 0,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 20,
+      trim_scope = "outer",
+      mode = "cursor",
+      separator = nil,
+      zindex = 50,
+      on_attach = nil,
+   },
 }
