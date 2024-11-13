1cfcad7709522359cec1cb22716edf9784dff51a
alexxGmZ <algilbertgomez@gmail.com>
2024-11-13 19:18:10 +0800

    catppuccin: higlight override for line number and gitsigns line blame

--- nvim/lua/plugins/colorscheme/catppuccin.lua
+++ nvim/lua/plugins/colorscheme/catppuccin.lua
@@ -25,6 +25,9 @@ return {
                ["@markup.heading.4.markdown"] = { fg = colors.green, bold = true },
                ["@markup.heading.5.markdown"] = { fg = colors.blue, bold = true },
                ["@markup.heading.6.markdown"] = { fg = colors.mauve, bold = true },
+               ["CursorLineNr"] = { fg = colors.yellow, bold = true },
+               ["LineNr"] = { fg = colors.overlay0 },
+               ["GitSignsCurrentLineBlame"] = { fg = colors.overlay0 },
             }
          end
       })
