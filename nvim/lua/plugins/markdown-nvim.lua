17534f96ee527cba6f96a42a2950a34fd1414c04
alexxGmZ <algilbertgomez@gmail.com>
2024-11-14 15:31:32 +0800

    markdown-nvim: laod by command

--- nvim/lua/plugins/markdown-nvim.lua
+++ nvim/lua/plugins/markdown-nvim.lua
@@ -1,7 +1,7 @@
 return {
    "MeanderingProgrammer/markdown.nvim",
    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-   ft = "markdown",
+   cmd = "RenderMarkdown",
    dependencies = { "nvim-treesitter/nvim-treesitter" },
    config = function()
       require("render-markdown").setup({
