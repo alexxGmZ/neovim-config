fc83c2e7df918375ebf9b63f8a4872f22e58e2ac
alexxGmZ <algilbertgomez@gmail.com>
2024-11-15 21:50:20 +0800

    player-nvim: add keymaps for next and play-pause

--- nvim/lua/plugins/player-nvim.lua
+++ nvim/lua/plugins/player-nvim.lua
@@ -4,6 +4,10 @@ return {
    -- dir = "$HOME/Git/player.nvim",
    -- cmd = "Player",
    event = "VeryLazy",
+   keys = {
+      { "<leader>pn", "<cmd>Player next<CR>", desc = "Player: Next track" },
+      { "<leader>pp", "<cmd>Player play-pause<CR>", desc = "Player: Play-pause" }
+   },
    config = function()
       require("player").setup({
          supported_players = {
