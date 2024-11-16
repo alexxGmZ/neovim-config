40f25a7309f667c7f7d23c5724ccf7b0750f7709
alexxGmZ <algilbertgomez@gmail.com>
2024-11-16 18:35:57 +0800

    nvim-tree: remove keymap

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,6 @@
 return {
    "nvim-tree/nvim-tree.lua",
    cmd = { "NvimTreeOpen", "NvimTreeToggle" },
-   keys = {
-      { "<leader>ff", "<cmd>NvimTreeToggle<CR>", desc = "NvimTree: Toggle" },
-   },
    config = function()
       local HEIGHT_RATIO = 0.8 -- You can change this
       local WIDTH_RATIO = 0.5  -- You can change this too
