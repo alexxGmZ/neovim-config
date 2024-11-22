a3bdeec2dbf013f409d86f53c34801a9bf4d1462
alexxGmZ <algilbertgomez@gmail.com>
2024-11-22 10:01:13 +0800

    md2pdf: separate plugin file

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,16 +1,5 @@
 return {
    {
-      -- "alexxGmZ/Md2Pdf",
-      -- cmd = "Md2Pdf",
-      dir = "$HOME/Git/Md2Pdf",
-      config = function()
-         require("Md2Pdf").setup({
-            pdf_engine = "xelatex"
-         })
-      end
-   },
-
-   {
       "sitiom/nvim-numbertoggle",
       enabled = true,
       event = "VeryLazy",
