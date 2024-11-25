dda7e6adf7a4758699f2641aa1b9b8a48daf2783
alexxGmZ <algilbertgomez@gmail.com>
2024-11-25 15:52:24 +0800

    everforest as main colorscheme

--- nvim/lua/plugins/colorscheme/catppuccin.lua
+++ nvim/lua/plugins/colorscheme/catppuccin.lua
@@ -1,8 +1,8 @@
 return {
    "catppuccin/nvim",
    name = "catppuccin",
-   priority = 1000,
-   -- lazy = true,
+   -- priority = 1000,
+   lazy = true,
    config = function()
       require("catppuccin").setup({
          flavour = "auto",
