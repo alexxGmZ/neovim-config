8316cc95cdac4d822c28d077092048a7dad72424
alexxGmZ <algilbertgomez@gmail.com>
2024-12-08 16:58:11 +0800

    add highlight to man page

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -41,7 +41,7 @@ create_autocmd({ "CmdlineLeave", "UIEnter" }, {
 
 -- "syntax on" to some filetypes. "syntax" is off by default in order to utilize treesitter
 create_autocmd("FileType", {
-   pattern = { "html", "tmux", "fugitive", "sh", "zsh", "oil", "git" },
+   pattern = { "html", "tmux", "fugitive", "sh", "zsh", "oil", "git", "man" },
    group = user_augroup,
    command = "syntax on"
 })
