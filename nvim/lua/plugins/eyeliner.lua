296ccc97590467c23bcc962a8d04d01e787cc691
alexxGmZ <algilbertgomez@gmail.com>
2025-02-06 13:23:58 +0800

    eyeliner: lazy load

--- nvim/lua/plugins/eyeliner.lua
+++ nvim/lua/plugins/eyeliner.lua
@@ -1,6 +1,7 @@
 return {
    "jinh0/eyeliner.nvim",
    -- cmd = { "EyelinerEnable", "EyelinerToggle" },
+   event = "VeryLazy",
    config = function ()
       require("eyeliner").setup({
          highlight_on_key = true,
