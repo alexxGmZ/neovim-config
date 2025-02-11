0797862a8a2ce455d5dfb468e737183de89c1f41
alexxGmZ <algilbertgomez@gmail.com>
2025-02-11 15:19:57 +0800

    spell: use `setlocal` instead of `set`

--- nvim/lua/plugins/cmp-spell.lua
+++ nvim/lua/plugins/cmp-spell.lua
@@ -8,17 +8,17 @@ return {
 
       -- enable spell
       vim.api.nvim_create_user_command("Spell", function()
-         vim.cmd("set spell")
+         vim.cmd("setlocal spell")
       end, {})
 
       -- disble spell
       vim.api.nvim_create_user_command("NoSpell", function()
-         vim.cmd("set nospell")
+         vim.cmd("setlocal nospell")
       end, {})
 
       -- toggle spell
       vim.api.nvim_create_user_command("SpellToggle", function()
-         vim.cmd("set invspell")
+         vim.cmd("setlocal invspell")
       end, {})
    end,
 }
