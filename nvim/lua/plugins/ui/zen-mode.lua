39f63dc2125a0bd5844e1361d4c0a286349de637
alexxGmZ <algilbertgomez@gmail.com>
2025-02-14 12:38:44 +0800

    zen-mode: clean options

--- nvim/lua/plugins/ui/zen-mode.lua
+++ nvim/lua/plugins/ui/zen-mode.lua
@@ -9,15 +9,7 @@ return {
          backdrop = 0.95,
          width = 100,
          height = 1,
-         options = {
-            -- signcolumn = "no", -- disable signcolumn
-            -- number = false, -- disable number column
-            -- relativenumber = false, -- disable relative numbers
-            -- cursorline = false, -- disable cursorline
-            -- cursorcolumn = false, -- disable cursor column
-            -- foldcolumn = "0", -- disable fold column
-            -- list = false, -- disable whitespace characters
-         },
+         options = {},
       },
       plugins = {
          options = {
