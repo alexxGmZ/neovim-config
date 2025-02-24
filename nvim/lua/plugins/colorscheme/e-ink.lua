24d75764157b01d7cfa3c03c35ce9fc49b2236b7
alexxGmZ <algilbertgomez@gmail.com>
2025-02-25 01:33:44 +0800

    e-ink: config

--- nvim/lua/plugins/colorscheme/e-ink.lua
+++ nvim/lua/plugins/colorscheme/e-ink.lua
@@ -1,10 +1,29 @@
+local function highlight_overrides()
+   local set_hl = vim.api.nvim_set_hl
+   local mono = require("e-ink.palette").mono
+
+   set_hl(0, "Function", { fg = mono[16], bold = true })
+   set_hl(0, "@variable", { fg = mono[16], bold = true })
+end
+
 return {
-   "alexxGmZ/e-ink.nvim",
    -- dir = "/home/alex/Git/e-ink.nvim",
-   -- branch = "beta",
+   "alexxGmZ/e-ink.nvim",
+   branch = "beta",
    priority = 1000,
    config = function()
-      require("e-ink").setup()
       vim.cmd.colorscheme "e-ink"
+
+      highlight_overrides()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+         group = "HANDSOME",
+         pattern = "*",
+         callback = function ()
+            if vim.g.colors_name ~= "e-ink" then
+               return
+            end
+            highlight_overrides()
+         end
+      })
    end
 }
