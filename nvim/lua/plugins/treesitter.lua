16b366d9c2f8f4e215d24724530b4641c2986a73
alexxGmZ <algilbertgomez@gmail.com>
2025-03-06 19:06:08 +0800

    treesitter: enable for html

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -32,7 +32,7 @@ return {
             },
             highlight = {
                enable = true,
-               disable = { "html", "csv", "tmux" },
+               disable = { "csv", "tmux" },
                additional_vim_regex_highlighting = false,
             },
             incremental_selection = { enable = false },
