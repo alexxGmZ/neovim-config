2928a8d94d422c159d0a249ae63e40c9fdafaff0
alexxGmZ <algilbertgomez@gmail.com>
2025-03-26 18:41:08 +0800

    usercmds: spell usercmds

--- nvim/lua/config/usercmds.lua
+++ nvim/lua/config/usercmds.lua
@@ -11,3 +11,21 @@ create_user_command("DiffOrig", function()
    vim.cmd.wincmd("p")
    vim.cmd.diffthis()       -- current buffer
 end, {})
+
+vim.opt.spell = false
+vim.opt.spelllang = { 'en_us' }
+
+-- enable spell
+vim.api.nvim_create_user_command("Spell", function()
+   vim.cmd("setlocal spell")
+end, {})
+
+-- disble spell
+vim.api.nvim_create_user_command("NoSpell", function()
+   vim.cmd("setlocal nospell")
+end, {})
+
+-- toggle spell
+vim.api.nvim_create_user_command("SpellToggle", function()
+   vim.cmd("setlocal invspell")
+end, {})
