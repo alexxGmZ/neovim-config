bf5da34053f7b647aa626f501a5018e55d00d2a2
alexxGmZ <algilbertgomez@gmail.com>
2025-03-27 11:56:45 +0800

    blink-cmp: single quote to double quote

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,40 +1,40 @@
 return {
-   'saghen/blink.cmp',
+   "saghen/blink.cmp",
    event = { "CmdlineEnter", "InsertEnter" },
    dependencies = {
-      'rafamadriz/friendly-snippets',
-      'hrsh7th/nvim-cmp',
+      "rafamadriz/friendly-snippets",
+      "hrsh7th/nvim-cmp",
    },
-   version = '1.*',
+   version = "1.*",
    opts = {
       keymap = {
-         preset = 'enter',
-         ['<S-Tab>'] = { 'select_prev', 'fallback' },
-         ['<Tab>'] = { 'select_next', 'fallback' },
+         preset = "enter",
+         ["<S-Tab>"] = { "select_prev", "fallback" },
+         ["<Tab>"] = { "select_next", "fallback" },
       },
 
       appearance = {
-         nerd_font_variant = 'normal'
+         nerd_font_variant = "normal"
       },
 
       completion = {
-         menu = { border = 'rounded' },
+         menu = { border = "rounded" },
          documentation = {
             auto_show = true,
-            window = { border = 'rounded' }
+            window = { border = "rounded" }
          },
          list = {
             selection = { preselect = false, auto_insert = true },
          }
       },
       sources = {
-         default = { 'lsp', 'path', 'snippets', 'buffer' },
+         default = { "lsp", "path", "snippets", "buffer" },
       },
       fuzzy = { implementation = "prefer_rust_with_warning" },
       cmdline = {
          keymap = {
-            preset = 'inherit',
-            ['<CR>'] = { 'accept_and_enter', 'fallback' }
+            preset = "inherit",
+            ["<CR>"] = { "accept_and_enter", "fallback" }
          },
          completion = {
             menu = { auto_show = true },
