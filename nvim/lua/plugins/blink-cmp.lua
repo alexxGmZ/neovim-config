75f762492500a5658eddd3d0cfd5509509711eea
alexxGmZ <algilbertgomez@gmail.com>
2025-04-19 21:27:40 +0800

    blink-cmp: vim-dadbod-completion source

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -29,6 +29,12 @@ return {
       },
       sources = {
          default = { "lsp", "path", "snippets", "buffer" },
+         per_filetype = {
+            sql = { 'snippets', 'dadbod', 'buffer' },
+         },
+         providers = {
+            dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+         },
       },
       fuzzy = { implementation = "prefer_rust" },
       cmdline = {
