2ad622116ab5d6ee82db7d2f82021b1510aaf7b1
alexxGmZ <algilbertgomez@gmail.com>
2025-04-26 18:37:13 +0800

    vim-rest-console: comment out buffer name and default content type

--- nvim/lua/plugins/vim-rest-console.lua
+++ nvim/lua/plugins/vim-rest-console.lua
@@ -3,8 +3,8 @@ return {
    ft = "rest",
    init = function()
       vim.g.vrc_set_default_mapping = 0
-      vim.g.vrc_response_default_content_type = "application/json"
-      vim.g.vrc_output_buffer_name = "response.json"
+      -- vim.g.vrc_response_default_content_type = "application/json"
+      -- vim.g.vrc_output_buffer_name = "response.json"
       vim.g.vrc_auto_format_response_enabled = 1
       vim.g.vrc_auto_format_response_patterns = {
          json = "jq"
