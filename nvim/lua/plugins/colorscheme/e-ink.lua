4814fb09a2f8b2f139c4fd8fb159bb58d684488a
alexxGmZ <algilbertgomez@gmail.com>
2025-04-27 20:59:39 +0800

    e-ink: transparent background, remove lazy.nvim highlights

--- nvim/lua/plugins/colorscheme/e-ink.lua
+++ nvim/lua/plugins/colorscheme/e-ink.lua
@@ -6,10 +6,10 @@ local function highlight_overrides()
    -- print(vim.inspect(everforest))
 
    -- transparent when dark background
-   -- if vim.o.background == "dark" then
-   --    set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
-   --    set_hl(0, "NotifyBackground", { bg = mono[1] })
-   -- end
+   if vim.o.background == "dark" then
+      set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
+      set_hl(0, "NotifyBackground", { bg = mono[1] })
+   end
 
    set_hl(0, "EyelinerPrimary", { fg = everforest.orange, reverse = true, bold = true })
    set_hl(0, "EyelinerSecondary", { fg = everforest.blue, reverse = true, bold = true })
@@ -17,18 +17,6 @@ local function highlight_overrides()
    set_hl(0, "@variable", { fg = mono[16], bold = true })
    set_hl(0, "Pmenu", { link = "NormalFloat" })
    set_hl(0, "Type", { fg = mono[13], bold = true })
-
-   set_hl(0, "LazyCommitIssue", { fg = everforest.orange, bold = true })
-   set_hl(0, "LazyReasonCmd", { fg = everforest.yellow })
-   set_hl(0, "LazyReasonEvent", { fg = everforest.orange })
-   set_hl(0, "LazyReasonFt", { fg = everforest.green })
-   set_hl(0, "LazyReasonImport", { fg = everforest.blue })
-   set_hl(0, "LazyReasonIssue", { fg = everforest.orange })
-   set_hl(0, "LazyReasonKeys", { fg = everforest.red })
-   set_hl(0, "LazyReasonPlugin", { fg = everforest.yellow })
-   set_hl(0, "LazyReasonRequire", { fg = everforest.red })
-   set_hl(0, "LazyReasonSource", { fg = everforest.aqua })
-   set_hl(0, "LazyReasonStart", { fg = everforest.blue })
 
    set_hl(0, "@markup.list.checked", { fg = everforest.green, bold = true })
    set_hl(0, "@markup.list.unchecked", { fg = everforest.red, bold = true })
