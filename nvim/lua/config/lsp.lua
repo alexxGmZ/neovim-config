4c3597907cfa88bef18e5b0372a3377e9557cb24
alexxGmZ <algilbertgomez@gmail.com>
2025-10-16 17:24:33 +0800

    lsp: comment out default format keymap

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -66,9 +66,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
       vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<leader>f", function()
-         vim.lsp.buf.format { async = true }
-      end, opts)
+      -- vim.keymap.set("n", "<leader>f", function()
+      --    vim.lsp.buf.format { async = true }
+      -- end, opts)
    end,
 })
 
