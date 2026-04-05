e3aa776674ca380cf4d3dfb2a088bd976619db9f
alexxGmZ <algilbertgomez@gmail.com>
2026-04-05 12:04:35 +0800

    lsp: new configuation for diagnostic signs

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -74,21 +74,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 --
--- diagnostic symbols
---
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
-
-for type, icon in pairs(signs) do
-   local hl = "DiagnosticSign" .. type
-   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
---
 -- diagnostic config
 --
 vim.diagnostic.config({
    virtual_text = false,
-   signs = true,
+   signs = {
+      text = {
+         [vim.diagnostic.severity.ERROR] = '󰅚 ',
+         [vim.diagnostic.severity.WARN] = '󰀪 ',
+         [vim.diagnostic.severity.INFO] = '󰋽 ',
+         [vim.diagnostic.severity.HINT] = '󰌶 ',
+      }
+   },
    underline = true,
    update_in_insert = false,
    severity_sort = true,
