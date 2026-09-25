926ad89057b98531996651953bd8658b1c185ab5
alexxGmZ <algilbertgomez@gmail.com>
2026-09-25 22:01:02 +0800

    markdown-preview: new repository

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,22 +1,11 @@
 return {
-   "iamcco/markdown-preview.nvim",
+   "sammaji/markdown-preview.nvim",
    cmd = {
       "MarkdownPreviewToggle",
       "MarkdownPreview",
       "MarkdownPreviewStop",
    },
    ft = { "markdown" },
-   build = function(plugin)
-      if vim.fn.executable "npx" then
-         vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-      else
-         vim.cmd [[Lazy load markdown-preview.nvim]]
-         vim.fn["mkdp#util#install"]()
-      end
-   end,
-   init = function()
-      if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
-   end,
    config = function()
       local g = vim.g
       g.mkdp_refresh_slow = 1
